774d5b4f735511ab4514d65f5ac8ce6d5e166645
opot <youtolife@yandex.ru>
2014-12-24 18:40:43 +0700

    init commit

---
+++ desktop/src/com/youtoolife/labyrinth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.youtoolife.labyrinth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.youtoolife.labyrinth.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
