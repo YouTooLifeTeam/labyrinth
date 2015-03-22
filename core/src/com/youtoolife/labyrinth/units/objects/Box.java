9c787cebe33cc9440a08d1358c6e1de04fad4fa0
opot <youtolife@yandex.ru>
2015-03-22 23:21:56 +0700

    Small sync commit

---
+++ core/src/com/youtoolife/labyrinth/units/objects/Box.java
@@ -0,0 +1,26 @@
+package com.youtoolife.labyrinth.units.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.youtoolife.labyrinth.controller.Controller;
+import com.youtoolife.labyrinth.units.Unit;
+
+public class Box extends Unit{
+
+	public Box(int ChunkX, int ChunkY, Controller control) {
+		super(ChunkX, ChunkY, control);
+	}
+
+	@Override
+	public void draw(SpriteBatch batch, float x, float y){
+		
+	}
+	
+	@Override
+	public void update(){
+		
+	}
+	
+	@Override
+	public void useAbility() {}
+
+}
